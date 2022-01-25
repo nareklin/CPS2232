87225a67cc6b09a74b4cc128513c74393619aa2f
nickn <nickn@10.0.0.63>
2022-01-25 14:23:37 -0500

    Iinitial Project Commit

---
+++ CPS2232-02/src/Program1.java
@@ -0,0 +1,16 @@
+/*
+ * 	Course Section: CPS2232-02
+ * 	Author: Nika Nareklishvili
+ * 	Email: nareklin@kean.edu
+ */
+
+public class Program1 {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
